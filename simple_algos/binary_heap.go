d95a35698feff7f7d93efd3f8da21f1f8d5f8ff4
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-04-02 11:27:01 +0200

    Fix binary hep implementation for max-heap.

--- simple_algos/binary_heap.go
+++ simple_algos/binary_heap.go
@@ -61,7 +61,7 @@ func (heap *BinaryHeap[T]) heapifyDown(index int) {
 	for leftIndex <= lastIndex {
 		if leftIndex == lastIndex { // When left child is the only child
 			indexToCompare = leftIndex
-		} else if heap.elements[leftIndex] < heap.elements[rightIndex] {
+		} else if heap.comparator.Compare(heap.elements[leftIndex], heap.elements[rightIndex]) {
 			indexToCompare = leftIndex
 		} else {
 			indexToCompare = rightIndex
@@ -94,72 +94,8 @@ func (heap *BinaryHeap[T]) heapifyUp(index int) {
 	}
 }
 
-type MinHeap struct {
-	slice []int
-}
-
-func (heap *MinHeap) swap(i, j int) {
-	heap.slice[i], heap.slice[j] = heap.slice[j], heap.slice[i]
-}
-
-func (heap *MinHeap) Insert(key int) {
-	heap.slice = append(heap.slice, key)
-	heap.heapifyUp(len(heap.slice) - 1)
-}
-
-func (heap *MinHeap) heapifyUp(index int) {
-	for heap.slice[parent(index)] > heap.slice[index] {
-		heap.swap(parent(index), index)
-		index = parent(index)
-	}
-}
-
 func parent(index int) int {
 	return (index - 1) / 2
-}
-
-func (heap *MinHeap) Pop() (int, error) {
-	if len(heap.slice) == 0 {
-		return 0, errors.New("MinHeap is empty")
-	}
-
-	minimum := heap.slice[0]
-	heap.slice[0] = heap.slice[len(heap.slice)-1]
-	heap.slice = heap.slice[:len(heap.slice)-1]
-	heap.heapifyDown(0)
-	return minimum, nil
-}
-
-func (heap *MinHeap) Top() (int, error) {
-	if len(heap.slice) == 0 {
-
-		return 0, errors.New("MinHeap is empty")
-	}
-	return heap.slice[0], nil
-}
-
-func (heap *MinHeap) heapifyDown(index int) {
-	lastIndex := len(heap.slice) - 1
-	leftIndex, rightIndex := leftChild(index), rightChild(index)
-	indexToCompare := 0
-
-	for leftIndex <= lastIndex {
-		if leftIndex == lastIndex { // When left child is the only child
-			indexToCompare = leftIndex
-		} else if heap.slice[leftIndex] < heap.slice[rightIndex] {
-			indexToCompare = leftIndex
-		} else {
-			indexToCompare = rightIndex
-		}
-
-		if heap.slice[index] > heap.slice[indexToCompare] {
-			heap.swap(index, indexToCompare)
-			index = indexToCompare
-			leftIndex, rightIndex = leftChild(index), rightChild(index)
-		} else {
-			return
-		}
-	}
 }
 
 func leftChild(index int) int {
--- test_simple_algos/binary_heap_test.go
+++ test_simple_algos/binary_heap_test.go
@@ -6,40 +6,40 @@ import (
 	"github.com/Schwarf/go_basics/simple_algos"
 )
 
-func TestInsertAndRemoveBinaryHeap(t *testing.T) {
+func TestInsertAndRemoveMinHeap(t *testing.T) {
+	minHeap := simple_algos.NewBinaryHeap[int](simple_algos.Less[int]{})
+	valuesToInsert := []int{5, 3, 8, 1, 2}
+
+	for _, val := range valuesToInsert {
+		minHeap.Insert(val)
+	}
+
+	expectedOrder := []int{1, 2, 3, 5, 8}
+	for _, expected := range expectedOrder {
+		if value, error := minHeap.Pop(); error != nil || expected != value {
+			t.Errorf("Heap did not remove elements in expected order.")
+		}
+	}
+}
+
+func TestInsertAndRemoveMaxHeap(t *testing.T) {
+	maxHeap := simple_algos.NewBinaryHeap[float64](simple_algos.Greater[float64]{})
+	valuesToInsert := []float64{5, 3, 8, 1, 2}
+
+	for _, val := range valuesToInsert {
+		maxHeap.Insert(val)
+	}
+
+	expectedOrder := []float64{8, 5, 3, 2, 1}
+	for _, expected := range expectedOrder {
+		if value, error := maxHeap.Pop(); error != nil || expected != value {
+			t.Errorf("Heap did not remove elements in expected order. Expected: %.2f, Is: %.2f", expected, value)
+		}
+	}
+}
+
+func TestRemoveEmptyMinHeap(t *testing.T) {
 	heap := simple_algos.NewBinaryHeap[int](simple_algos.Less[int]{})
-	valuesToInsert := []int{5, 3, 8, 1, 2}
-
-	for _, val := range valuesToInsert {
-		heap.Insert(val)
-	}
-
-	expectedOrder := []int{1, 2, 3, 5, 8}
-	for _, expected := range expectedOrder {
-		if value, error := heap.Pop(); error != nil || expected != value {
-			t.Errorf("Heap did not remove elements in expected order.")
-		}
-	}
-}
-
-func TestInsertAndRemove(t *testing.T) {
-	heap := simple_algos.MinHeap{}
-	valuesToInsert := []int{5, 3, 8, 1, 2}
-
-	for _, val := range valuesToInsert {
-		heap.Insert(val)
-	}
-
-	expectedOrder := []int{1, 2, 3, 5, 8}
-	for _, expected := range expectedOrder {
-		if value, error := heap.Pop(); error != nil || expected != value {
-			t.Errorf("Heap did not remove elements in expected order.")
-		}
-	}
-}
-
-func TestRemoveEmpty(t *testing.T) {
-	heap := simple_algos.MinHeap{}
 	if value, error := heap.Pop(); error == nil || value != 0 {
 		t.Errorf("Empty heap does not return -1.")
 	}
