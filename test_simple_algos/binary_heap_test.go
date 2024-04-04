3cf4ce9598ebb441439c991801946a41b586d998
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-04-04 19:25:53 +0200

    Add test for method Top.

--- test_simple_algos/binary_heap_test.go
+++ test_simple_algos/binary_heap_test.go
@@ -44,3 +44,23 @@ func TestRemoveEmptyMinHeap(t *testing.T) {
 		t.Errorf("Empty heap does not return -1.")
 	}
 }
+
+func TestPopMinHeap(t *testing.T) {
+	minHeap := simple_algos.NewBinaryHeap[int8](simple_algos.Less[int8]{})
+	valuesToInsert := []int8{5, 3, 8, 1, 2}
+	for _, val := range valuesToInsert {
+		minHeap.Insert(val)
+	}
+
+	expectedOrder := []int8{1, 2, 3, 5, 8}
+	for _, expected := range expectedOrder {
+		if value, error := minHeap.Top(); error != nil || expected != value {
+			t.Errorf("Heap did not show elements in expected order. Expected: %.2d, Is: %.2d", expected, value)
+		}
+		if value, error := minHeap.Pop(); error != nil || expected != value {
+			t.Errorf("Heap did not remove elements in expected order. Expected: %.2d, Is: %.2d", expected, value)
+		}
+
+	}
+
+}
