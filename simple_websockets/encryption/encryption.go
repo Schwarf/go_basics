48a2352161c9f61567d197885282709a056260d9
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-06-02 18:25:49 +0200

    Add some encryption module.

---
+++ simple_websockets/encryption/encryption.go
@@ -0,0 +1,58 @@
+package encryption
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"errors"
+	"io"
+)
+
+func Encrypt(plainText string, key []byte) (string, error) {
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+	galoisCounterMode, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+	nonce := make([]byte, galoisCounterMode.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+	cipherText := galoisCounterMode.Seal(nonce, nonce, []byte(plainText), nil)
+	return hex.EncodeToString(cipherText), nil
+}
+
+func Decrypt(cipherText string, key []byte) (string, error) {
+	data, err := hex.DecodeString(cipherText)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(data) < nonceSize {
+		return "", errors.New("cipher text too short")
+	}
+
+	nonce, cipherText := data[:nonceSize], data[nonceSize:]
+	plainText, err := gcm.Open(nil, nonce, cipherText, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plainText), nil
+}
