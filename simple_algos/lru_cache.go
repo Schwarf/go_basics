adde61d104673e93866aa360bc7cf2bf61b5404d
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-07-22 17:40:44 +0200

    Add put method.

--- simple_algos/lru_cache.go
+++ simple_algos/lru_cache.go
@@ -21,10 +21,30 @@ func New[Key comparable, Value any](capacity int) *LRUCache[Key, Value] {
 }
 
 func (lruCache *LRUCache[Key, Value]) Get(key Key) (Value, bool) {
+	// if element in the list move, it to front and return it
 	if element, ok := lruCache.cache[key]; ok {
 		lruCache.list.MoveToFront(element)
 		return element.Value.(lruCacheEntry[Key, Value]).value, true
 	}
 	var zero Value // is nil (where nil is allowed) and 0 (false) for others like int(bool)
 	return zero, false
+}
+
+func (lruCache *LRUCache[Key, Value]) Put(key Key, value Value) {
+	// if element already in the list move it to front
+	if element, ok := lruCache.cache[key]; ok {
+		lruCache.list.MoveToFront(element)
+		element.Value = lruCacheEntry[Key, Value]{key, value}
+	}
+	// Delete last element if cache is full
+	if lruCache.list.Len() == lruCache.capacity {
+		back := lruCache.list.Back()
+		if back != nil {
+			lruCache.list.Remove(back)
+			delete(lruCache.cache, back.Value.(lruCacheEntry[Key, Value]).key)
+		}
+	}
+	// push new element to front
+	element := lruCache.list.PushFront(lruCacheEntry[Key, Value]{key, value})
+	lruCache.cache[key] = element
 }
