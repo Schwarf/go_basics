2deec02b6a4e6b2daa4f62817ba3787abdaba4fb
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-07-22 18:57:27 +0200

    Add some more tests.

--- test_simple_algos/lru_cache_test.go
+++ test_simple_algos/lru_cache_test.go
@@ -19,9 +19,38 @@ func TestLRUCacheLen(t *testing.T) {
 	}
 }
 
-func TestLRUCacheMostRecent(t *testing.T) {
+func TestLRUCachePeek(t *testing.T) {
 	length := 3
 	cache := simple_algos.NewLRUCache[int, int](length)
 	cache.Put(1, 1)
+
+	_, notOk := cache.Peek(2)
+	if notOk {
+		t.Errorf("Found an expected element")
+	}
+
+	_, ok := cache.Peek(1)
+	if !ok {
+		t.Errorf("Did not find an expected element")
+	}
+}
+
+func TestLRUCacheEvictPolicy(t *testing.T) {
+	length := 3
+	cache := simple_algos.NewLRUCache[int, int](length)
+	cache.Put(1, 1)
+	cache.Put(2, 2)
+	cache.Put(3, 3)
+	_, ok := cache.Peek(1)
+	if !ok {
+		t.Errorf("Did not find an expected element")
+	}
+
+	cache.Put(4, 4)
+
+	_, notOk := cache.Peek(1)
+	if notOk {
+		t.Errorf("Found an expected element")
+	}
 
 }
